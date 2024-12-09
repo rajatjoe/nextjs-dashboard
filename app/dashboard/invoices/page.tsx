c488a5adbea11ff7aa631760513eb46ebf21caa0
rajatjoe <119100839+rajatjoe@users.noreply.github.com>
2024-12-09 16:28:27 +0530

    add suspense for loading components

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-    return <p>Invoices Page</p>;
+    return (
+      <main className="min-h-screen w-screen">
+        <div className="flex justify-center items-center h-screen">
+          <h1 className="text-4xl font-bold">Page Not Found</h1>
+        </div>
+
+      </main>
+    )
   }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -60,6 +60,8 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+        //  PARALLEL FETCHING IS DONE HERE USING PROMISE.ALL
+
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
